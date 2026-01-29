96d8c77507aa17aff6da9fc0eb460db4ba690e67
Kauziin_ <74920731+Kauziin@users.noreply.github.com>
2026-01-29 08:06:02 -0300

    Update insert_sieve.js

--- BP/scripts/config/recipes/added/insert_sieve.js
+++ BP/scripts/config/recipes/added/insert_sieve.js
@@ -7,11 +7,11 @@ world.afterEvents.worldLoad.subscribe(() => {
             { item: "utilitycraft:titanium_chunk", amount: 1, chance: 0.1, tier: 4 }
         ],
         "utilitycraft:compressed_crushed_cobbled_deepslate": [
-            { item: "utilitycraft:aetherium_shard", amount: 1, chance: 0.005, tier: 7 },
-            { item: "utilitycraft:titanium_chunk", amount: 1, chance: 0.1, tier: 4 }
+            { item: "utilitycraft:aetherium_shard", amount: 9, chance: 0.005, tier: 7 },
+            { item: "utilitycraft:titanium_chunk", amount: 9, chance: 0.1, tier: 4 }
         ],
         "utilitycraft:crushed_endstone": [
-            { item: "utilitycraft:aetherium_shard", amount: 1, chance: 0.1, tier: 5 }
+            { item: "utilitycraft:aetherium_shard", amount: 9, chance: 0.1, tier: 5 }
         ],
         "utilitycraft:compressed_crushed_endstone": [
             { item: "utilitycraft:aetherium_shard", amount: 1, chance: 0.1, tier: 5 }
